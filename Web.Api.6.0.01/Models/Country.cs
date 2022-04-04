a39b43ff31ffb16e4a9c0869e386d8aebcd39335
ScreamForceOne <ScreamForceOne@SFO-PC>
2022-04-04 07:24:59 +0200

    test

--- Web.Api.6.0.01/Models/Country.cs
+++ Web.Api.6.0.01/Models/Country.cs
@@ -8,6 +8,6 @@
     public List<City> Cities { get; set; }
 
     return PhonebookContext.Applications
-     .Include(a => a.City.Select(c => c.CityRelationshipType));
+     .Include(a => a.City.Select(c => c.CityRelationshipType));    //ovo me navjise muci, izgubia sam masu vremena na ovome pls halp XD
 }
 
